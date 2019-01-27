4e400a59c9892c2468c5b39768e70d122c831d10
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-01-27 18:40:01 -0500

    added propsoal app

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'core',
 	'website',
+	'proposal',
 ]
 
 MIDDLEWARE = [
---
+++ proposal/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.urls import path
+# Application Routes (URLs)
+
+app_name = 'proposal'
+
+urlpatterns = [
+    	# General Page Views
+		]
