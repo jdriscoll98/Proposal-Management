d4987319ef2a9e2b25e65f1c7ad7fb2ae9595c9f
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-01-28 12:24:58 -0500

    added homepage view for proposal homepage

--- proposal/urls.py
+++ proposal/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.urls import path
+from proposal.views import HomepageView
+
+from django.views.generic.base import TemplateView
 # Application Routes (URLs)
 
 app_name = 'proposal'
 
 urlpatterns = [
     	# General Page Views
+		url(r'^$', HomepageView.as_view(), name='homepage'),
 		]
--- proposal/views.py
+++ proposal/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from proposal.models import Proposal
 
 # Create your views here.
+class HomepageView(TemplateView):
+    template_name = 'proposal/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'proposals' : Proposal.objects.filter(sent=False)
+        }
+        return context
