e5267ae1bf597615a0c74c5b870a639eae3fc1d7
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-02-01 14:25:29 -0500

    added votes model and changed proposal model

--- proposal/models.py
+++ proposal/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
 class Proposal(models.Model):
-    acceptance_choices = (
+    status_choices = (
+        ('revised', 'revised'),
+        ('sent', 'sent'),
         ('accepted', 'accepted'),
         ('pending', 'pending'),
-        ('denied', 'denied'),
+        ('rejected', 'rejected'),
     )
 
     name = models.CharField(max_length=100) #name to remember client by
@@ -18,9 +20,7 @@ class Proposal(models.Model):
     proposal_link = models.URLField(null=True) # link to proposal if online
     num_of_upvotes = models.IntegerField(default=0) # votes to send proposal to lient
     num_of_downvotes = models.IntegerField(default=0) # votes to ~not~ send proposal to lient
-    proposal_revised = models.BooleanField(default=False) # if the proposal has been revised
-    sent = models.BooleanField(default = False) # if the proposal has been sent
-    status = models.CharField(max_length=100, choices=acceptance_choices, default='pending') # status of proposal
+    status = models.CharField(max_length=100, choices=status_choices, default='pending') # status of proposal
 
     def __str__(self):
         return str(self.name)
@@ -34,7 +34,12 @@ class Proposal(models.Model):
     def has_comments(self):
         return Comment.objects.filter(proposal=self)
 
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    decision = models.CharField(max_length = 3, choices=(('Yes', 'Yes'), ('No', 'No')))
+    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+
 class Comment(models.Model):
     text = models.CharField(max_length=1000)
-    author = models.CharField(blank=True, max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
