5e7f684dc59ca37a536bda8a9d07683d6fbe8dc3
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-02-02 16:15:14 -0500

    can now change votes on a proposal

--- proposal/utils.py
+++ proposal/utils.py
@@ -4,22 +4,44 @@ def create_vote(user, proposal, decision):
     success = False
 
     try:
-        if not Vote.objects.filter(user=user, proposal=proposal).exists():
-            Vote.objects.create(user=user, proposal=proposal, decision=decision)
-            success = True
+        if not Vote.objects.filter(user=user, proposal=proposal, decision=decision).exists():
+            if not Vote.objects.filter(user=user, proposal=proposal).exists():
+                    Vote.objects.create(user=user, proposal=proposal, decision=decision)
+                    success = True
 
-            if decision.lower() == "yes":
-                proposal.num_of_upvotes += 1
+                    if decision.lower() == "yes":
+                        proposal.num_of_upvotes += 1
+                    else:
+                        proposal.num_of_downvotes += 1
+
+                    if proposal.ready_to_revise():
+                        proposal.accepted()
+
+                    elif proposal.denied_by_team():
+                        proposal.denied()
+
+                    proposal.save()
             else:
-                proposal.num_of_downvotes += 1
+                vote = Vote.objects.get(user=user, proposal=proposal)
+                vote.decision = decision
+                vote.save()
 
-            if Proposal.ready_to_revise():
-                proposal.accepted()
+                if decision.lower() == "yes":
+                    proposal.num_of_upvotes += 1
+                    proposal.num_of_downvotes -= 1
+                else:
+                    proposal.num_of_downvotes += 1
+                    proposal.num_of_upvotes -= 1
 
-            elif proposal.denied_by_team():
-                proposal.denied()
+                if proposal.ready_to_revise():
+                    proposal.accepted()
 
-            proposal.save()
+                elif proposal.denied_by_team():
+                    proposal.denied()
+
+                proposal.save()
+                
+                success = True
 
     except Exception as e:
         print(str(e))
